6c09c6954d663d706e11c900fe0686326e3c4a10
ali <vick12052002@gmail.com>
2021-01-25 23:58:44 +0800

    表單驗證

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <h1 class="header-title">購物清單</h1>
     <main class="container">
-      <AddItem :input="input" @click="addItem" />
+      <AddItem
+        :input="input"
+        @handleClick="addItem"
+        @handleChange="checkInputsValid"
+        @handleKeyup="checkInputsValid"
+      />
       <ItemLists :itemList="itemList" @delete="deleteItem($event)" />
       <div class="item-list-container">
         <div class="total-container">
@@ -12,12 +17,13 @@
     </main>
   </div>
 </template>
-
+ƒ
 <script>
 import '../public/normalize.css';
 import '../public/index.css';
 import AddItem from './components/AddItem.vue';
 import ItemLists from './components/ItemLists.vue';
+import { isPriceValid, isAmountValid } from './utilis';
 
 export default {
   name: 'App',
@@ -27,6 +33,8 @@ export default {
   },
   methods: {
     addItem() {
+      this.checkInputsValid();
+      if (Object.values(this.input.hasError).includes(true)) return;
       if (this.lastId.length === 0) {
         this.lastId = this.itemList[this.itemList.length - 1].id;
       }
@@ -42,11 +50,16 @@ export default {
         name: '',
         price: 0,
         amount: 0,
+        hasError: {
+          name: false,
+          amount: false,
+          price: false,
+        },
       };
       this.countTotal();
     },
     deleteItem(id) {
-      const newList = this.itemList.filter((item) => item.id !== id);
+      const newList = this.itemList.fＦＦＦilter((item) => item.id !== id);
       this.itemList = newList;
       this.countTotal();
     },
@@ -57,19 +70,41 @@ export default {
       const itemsCount = this.itemList.map((item) => item.price * item.amount);
       this.total = itemsCount.reduce((accum, curr) => accum + curr);
     },
+    checkInputsValid() {
+      if (this.input.name.length > 20 || this.input.name.length === 0) {
+        this.input.hasError.name = true;
+      } else {
+        this.input.hasError.name = false;
+      }
+
+      if (this.input.price === 0 || !isPriceValid(this.input.price)) {
+        this.input.hasError.price = true;
+      } else {
+        this.input.hasError.price = false;
+      }
+
+      if (this.input.amount === 0 || !isAmountValid(this.input.amount)) {
+        this.input.hasError.amount = true;
+      } else {
+        this.input.hasError.amount = false;
+      }
+    },
   },
   data() {
     return {
-      itemList: [
-      ],
+      itemList: [],
       input: {
         name: '',
         price: 0,
         amount: 0,
+        hasError: {
+          name: false,
+          amount: false,
+          price: false,
+        },
       },
       total: 0,
       lastId: 0,
-      hasError: false,
     };
   },
 };
